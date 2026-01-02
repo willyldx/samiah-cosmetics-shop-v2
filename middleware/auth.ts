ef9af5b3f9fe14b0dda7f52c9f4d96f7ecd1ffa7
Willy <spencer62@protonmail.com>
2026-01-02 10:32:37 +0100

    Update auth.ts

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,9 +1,21 @@
-export default defineNuxtRouteMiddleware(async (to) => {
-  const supabase = useSupabaseClient()
+export default defineNuxtRouteMiddleware((to) => {
+  // Cette fonction est instantanée (pas besoin de 'await')
+  const user = useSupabaseUser()
+
+  // CAS 1 : L'utilisateur n'est PAS connecté
+  if (!user.value) {
+    // S'il essaie d'aller sur une page admin, mais n'est pas sur le login
+    if (to.path !== '/admin/login') {
+      return navigateTo('/admin/login')
+    }
+  } 
   
-  const { data: { session } } = await supabase.auth.getSession()
-  
-  if (!session) {
-    return navigateTo('/admin/login')
+  // CAS 2 : L'utilisateur EST connecté
+  else {
+    // S'il essaie d'aller sur le login alors qu'il est déjà connecté
+    // On le redirige vers le Dashboard pour gagner du temps
+    if (to.path === '/admin/login') {
+      return navigateTo('/admin') // ou '/admin/dashboard' selon ta route
+    }
   }
 })
